270384e18983810eb604ea2f969f6e9dc65e6662
Luis Ramos <luisramosmaldonado@gmail.com>
2023-07-31 01:09:28 -0600

    Chapter 3 challenges to print to the console done

---
+++ Chapter_03/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Rilin!");
+Console.WriteLine("Second statement");
+Console.WriteLine("More printing");
+Console.WriteLine("Time to go");
+Console.WriteLine("Bye now");
