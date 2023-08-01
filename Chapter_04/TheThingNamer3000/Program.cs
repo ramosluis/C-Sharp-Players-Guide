65e1cd22b753dc920758a2e266ae8c32d5a38aea
Luis Ramos <luisramosmaldonado@gmail.com>
2023-07-31 21:49:03 -0600

    Completed challenge in chapter 4 to use comments in code

---
+++ Chapter_04/TheThingNamer3000/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+/* Store the name of the object in question into variable a */
+string a = Console.ReadLine();
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // store the description of the object in b
+string c = "of Doom"; // stores a string literal in c
+string d = "3000"; // stores a string literal in d
+
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
