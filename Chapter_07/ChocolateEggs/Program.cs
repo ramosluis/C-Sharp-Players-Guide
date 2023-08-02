410bc45cc966be99fbe6922dc5a9b6a063ebf90c
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-01 21:22:17 -0600

    Finished Chapter 07 Challenges

---
+++ Chapter_07/ChocolateEggs/Program.cs
@@ -0,0 +1,6 @@
+﻿int eggs = 0;
+
+Console.WriteLine("How many chocolate eggs were gathered today?");
+eggs = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Each sister gets " + eggs / 4 + " eggs.");
+Console.WriteLine("Therefore, the duckbear gets " + eggs % 4 + " eggs.");
---
+++ Chapter_07/KingdomWorthCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter the number of provinces: ");
+int provinces = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of duchies: ");
+int duchies  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of estates: ");
+int estates  = Convert.ToInt32(Console.ReadLine());
+
+provinces *= 6;
+duchies *= 3;
+
+int total = provinces + duchies + estates;
+
+Console.WriteLine("Kingdom total value: " + total);
