1b5e01f0eed09abac0d2c2e4614584dc8c1de02b
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-01 23:06:50 -0600

    Finished Chapter 09 exercises
    Exercises dealt with basic program flow with if statements

---
+++ Chapter_09/Watchtower/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ( x < 0 )
+{
+    if (y > 0)
+    {
+        Console.WriteLine("The enemy is to the northwest!");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("The enemy is to the west!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the southwest!");
+    }
+}
+else if (x == 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("The enemy is to the north!");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("The enemy is here!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the south!");
+    }
+}
+else
+{
+    if (y > 0)
+    {
+        Console.WriteLine("The enemy is to the northeast!");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("The enemy is east!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the southeast!");
+    }
+}
