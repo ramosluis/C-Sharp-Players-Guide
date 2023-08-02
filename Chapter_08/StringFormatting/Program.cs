ebf66de192528bfdd6fc2679ef2e738f7e1efc3f
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-01 22:29:02 -0600

    Finished Chapter 08 challenge
    Exercise consisted of formatting strings and the console

---
+++ Chapter_08/StringFormatting/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Title = "Defense of Consolas";
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Clear();
+Console.Write("Target Row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Deploy to:");
+
+Console.WriteLine($"({row}, {column - 1})");
+Console.WriteLine($"({row - 1}, {column})");
+Console.WriteLine($"({row}, {column + 1})");
+Console.WriteLine($"({row + 1}, {column})");
+Console.Beep();
