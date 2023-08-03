c53f97ded8a12bf5a974f13d4b4ccd691cb6fca9
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-02 23:50:43 -0600

    Chapter 12 exercises
    Basic exercises to learn arrays and the foreach loop in C#

---
+++ Chapter_12/ArrayReplicator/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[5];
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write("Enter a number for the array: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    array[i] = input;
+}
+
+int[] arrayCopy = new int[5];
+
+Console.Clear();
+
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy[i] = array[i];
+    Console.WriteLine($"array{i}: {array[i]}");
+    Console.WriteLine($"arrayCopy{i}: {arrayCopy[i]}\n");
+}
---
+++ Chapter_12/Freach/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue; // Start higher than anything in the array
+int total = 0;
+
+foreach (int number in array)
+{
+    if (number < currentSmallest)
+        currentSmallest = number;
+
+    total += number;
+}
+
+float average = (float)total / array.Length;
+
+Console.WriteLine($"Current smallest: {currentSmallest}");
+Console.WriteLine($"Average: {average}");
