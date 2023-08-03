fb3b3ce1025c161d2d81ef8a3a2a25a7ae3eca7f
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-02 22:31:02 -0600

    Finished Chapter 11 exercises
    Exercises dealt with implementing loops

---
+++ Chapter_11/MagicCannon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 15 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{i}: Combined");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
+    }
+    else if (i % 5 == 0 )
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Electric");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"{i}: Normal");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+}
---
+++ Chapter_11/ThePrototype/Program.cs
@@ -0,0 +1,38 @@
+﻿int number = 0, guess = 0;
+
+while (true)
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    if(number < 0 || number > 100)
+    {
+        Console.WriteLine("Invalid number.");
+        continue;
+    }
+
+    Console.Clear();
+    break;
+}
+
+Console.WriteLine("User 2, guess the number.");
+
+while (true)
+{
+    Console.Write("What is your next guess? ");
+    guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess > number)
+    {
+        Console.WriteLine($"{guess} is too high.");
+    }
+    else if (guess < number)
+    {
+        Console.WriteLine($"{guess} is too low");
+    }
+    else
+    {
+        Console.WriteLine("You guessed the number!");
+        break;
+    }
+}
