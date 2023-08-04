a016641762184943aba5d787983bb3c1c5c635e9
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-03 21:32:11 -0600

    Chapter 13 exercises
    Exercises dealing with method implementation and method recursion

---
+++ Chapter_13/AskForNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿int number = AskForNumber("Give me a number!");
+Console.WriteLine($"You entered {number}!");
+Console.WriteLine("\n\n\n");
+int numberInRange = AskForNumberInRange("Give me a number between 1 and 10!", 1, 10);
+Console.WriteLine($"You entered {numberInRange}!");
+
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int returnValue = Convert.ToInt32(Console.ReadLine());
+    return returnValue;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    int returnValue = 0;
+    while (true)
+    {
+        Console.WriteLine(text);
+        returnValue = Convert.ToInt32(Console.ReadLine());
+        if(returnValue < min || returnValue > max)
+        {
+            continue;
+        }
+        break;
+    }
+    return returnValue;
+}
---
+++ Chapter_13/Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿Countdown(10);
+
+void Countdown(int number)
+{
+    if ( number >= 1)
+    {
+        Console.WriteLine(number);
+        Countdown(number - 1);
+    }
+
+    return;
+}
