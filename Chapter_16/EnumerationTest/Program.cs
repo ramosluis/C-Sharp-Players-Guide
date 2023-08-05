a1bc91da9878e539546700d07f2cfa2c1d56427f
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-05 16:32:20 -0600

    Chapter 16 exercises
    Basic enumeration exercise

---
+++ Chapter_16/EnumerationTest/Program.cs
@@ -0,0 +1,42 @@
+﻿ChestState state = ChestState.Locked;
+string action = "";
+
+while (true)
+{
+    Console.Write($"The chest is {state}. What do you want to do? ");
+    action = Console.ReadLine();
+
+    if (action == "unlock" && state == ChestState.Locked)
+    {
+        state = ChestState.Closed;
+    }
+    else if (action == "open" && state == ChestState.Closed)
+    {
+        state = ChestState.Open;
+    }
+    else if (action == "close" &&  state == ChestState.Open)
+    {
+        state = ChestState.Closed;
+    }
+    else if (action == "lock" && state == ChestState.Closed)
+    {
+        state = ChestState.Locked;
+    }
+    else if (action == "quit")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid command");
+    }
+}
+
+
+
+enum ChestState
+{
+    Open,
+    Closed,
+    Locked
+}
