95170ac89d50360f4a79ff3180e0e2087b4e7fb0
Luis Ramos <luisramosmaldonado@gmail.com>
2023-08-12 21:27:50 -0600

    Chapter 29 exercise
    Created a basic Record to make a simple Sword class

---
+++ Chapter_29/Blacksmith/Program.cs
@@ -0,0 +1,28 @@
+﻿Sword original = new Sword(SwordMaterial.Iron, Gemstone.None, 100, 20);
+Sword firstCopy = original with { Material = SwordMaterial.Steel, Stone = Gemstone.Sapphire };
+Sword secondCopy = original with { Length = 75, Width = 10};
+
+Console.WriteLine($@"Original: {original}
+First copy: {firstCopy}
+Second copy: { secondCopy}");
+
+public enum SwordMaterial
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+public enum Gemstone
+{
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone,
+    None
+}
+
+public record Sword(SwordMaterial Material, Gemstone Stone, int Length, int Width);
